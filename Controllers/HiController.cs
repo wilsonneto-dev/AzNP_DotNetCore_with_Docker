4500a070d101861be1498b4d01e79fe79da61f48
Wilson Neto <contato@wilsonneto.com.br>
2020-07-01 08:51:26 -0300

    simple api example. route /hi

---
+++ Controllers/HiController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace core.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class HiController : ControllerBase
+  {
+    private readonly ILogger<HiController> _logger;
+
+    public HiController(ILogger<HiController> logger)
+    {
+      _logger = logger;
+    }
+
+    [HttpGet]
+    public String Get()
+    {
+      return "I'm here ok!";
+    }
+  }
+}
